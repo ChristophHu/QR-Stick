47b62f8e38712e69fa489094a54f2cdcdc56830b
christophhu <christoph.huschenhoefer@gmx.de>
2026-01-20 14:27:18 +0100

    Initial Commit

---
+++ QR-Stick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QR-Stick
+//
+//  Created by Christoph Huschenhöfer on 20.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QR-Stick/QR_StickApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_StickApp.swift
+//  QR-Stick
+//
+//  Created by Christoph Huschenhöfer on 20.01.26.
+//
+
+import SwiftUI
+
+@main
+struct QR_StickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
